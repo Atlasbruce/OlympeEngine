4ee10f24696ce951325f76c62311bb83c37a51e0
Atlasbruce <2962180+Atlasbruce@users.noreply.github.com>
2025-11-29 19:13:11 +0100

    update solutionparameters

--- OlympeBlueprintEditor/src/main.cpp
+++ OlympeBlueprintEditor/src/main.cpp
@@ -4,7 +4,7 @@
 #include "../include/Graph.h"
 #include "../include/imnodes_stub.h"
 
-/*int main(int argc, char** argv)
+int main(int argc, char** argv)
 {
     std::cout << "Olympe Blueprint Editor (stub)" << std::endl;
     imnodes_stub::Initialize();
